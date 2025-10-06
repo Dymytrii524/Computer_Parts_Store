4bb0d3dae7e6dd8c44db71bd3f220d7763320b0e
NENEPE <oleksandrprystavsky@gmail.com>
2025-10-06 19:09:40 +0200

    Інтегровано базу даних в структуру

---
+++ Forms/Models/Order.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace Computer_Parts_Store.Models
+{
+    /// Замовлення клієнта
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+        public string Status { get; set; } = "Нове";
+
+        // Посилання на клієнта
+        public int CustomerId { get; set; }
+        [ForeignKey("CustomerId")]
+        public virtual Customer Customer { get; set; }
+
+        // Список товарів у замовленні
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+
+        [NotMapped]
+        public decimal TotalAmount => OrderItems?.Sum(oi => oi.TotalPrice) ?? 0;
+
+        public Order()
+        {
+            OrderItems = new HashSet<OrderItem>();
+        }
+
+        public override string ToString() => $"Замовлення #{Id} - {TotalAmount} грн";
+    }
+}
